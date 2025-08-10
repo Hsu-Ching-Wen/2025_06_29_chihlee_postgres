7638ccfa5d80ca94dca6fb3c5dd9cca0e699e2b1
Hsu-Chig-Wen <joanne850111@gmail.com>
2025-08-10 08:24:26 +0000

    modify

--- lesson10/main.py
+++ lesson10/main.py
@@ -1,12 +1,18 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+import streamlit as st
 import datasource
+
+load_dotenv()
 
 def main():
     results = datasource.get_stations_names()
+    st.title("台鐵車站名稱列表")
     if results:
-        for station in results:
-            print(station)
+        st.table([[station] for station in results])
     else:
-        print("無法取得車站資料")
+        st.error("無法取得車站資料")
 
 if __name__ == "__main__":
     main()
