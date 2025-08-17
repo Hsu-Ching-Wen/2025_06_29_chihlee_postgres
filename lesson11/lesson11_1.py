c0c13ca306dcea06c46b174c75f7eeb7d7e83ce5
Hsu-Ching-Wen <joanne850111@gmail.com>
2025-08-17 03:01:16 +0000

    modify

--- lesson11/lesson11_1.py
+++ lesson11/lesson11_1.py
@@ -1,8 +1,11 @@
+# 當輸出df變數時,st.write()會自動執行
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+st.title("台鐵車站資訊")
+st.header("2023年各站進出人數")
